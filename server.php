8b6650143adc1edde46d49221eed6c8baf72c8f4
youssefennnaciri <41855985+youssefennnaciri@users.noreply.github.com>
2020-11-11 23:38:45 +0100

    se changes

--- server.php
+++ server.php
@@ -328,7 +328,7 @@ if(isset($_POST['sus'])){
       if (mysqli_num_rows($results) == 1) {
         $_SESSION['username'] = $username;
         $_SESSION['success'] = "You are now logged in";
-        header('location: location: https://cheikhzaid.herokuapp.com/admin/admin_espace.php');
+        header('location: https://cheikhzaid.herokuapp.com/admin/admin_espace.php');
         //echo " welcome";
       }else {
         array_push($errors, "Wrong username/password combination");
